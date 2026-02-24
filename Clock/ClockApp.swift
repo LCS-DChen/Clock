5b78fc4dbf20b5de3296d3fccab27a16dcec6bfc
Deyana Chen <dchen@lcs.on.ca>
2026-02-24 14:14:31 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by chen tianyi on 2026-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
