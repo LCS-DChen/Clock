16945763c7922c945f0ccb2a233aee7dafd493ea
Deyana Chen <dchen@lcs.on.ca>
2026-02-24 15:08:51 -0500

    Set the color scheme and tint.

---
+++ Clock/View/AlarmsView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmsView.swift
+//  Clock
+//
+//  Created by chen tianyi on 2026-02-24.
+//
+
+import SwiftUI
+
+struct AlarmsView: View {
+    var body: some View {
+        Text("Alarms")
+    }
+}
+
+#Preview {
+    AlarmsView()
+}
---
+++ Clock/View/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by chen tianyi on 2026-02-24.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text("World Clock!")
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
